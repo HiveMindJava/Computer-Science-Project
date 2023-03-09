ea82be0d84818a186549b55cf0b417da0e331ee6
rkg3913 <90530735+rkg3913@users.noreply.github.com>
2023-03-09 15:57:25 -0600

    Add files via upload

---
+++ DegreePlanProject/DegreePlanProject/src/main/java/MainDriver.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+//import java.util.*;
+
+/**
+ * This class is the main driver of the application. 
+ * 
+ */
+public class MainDriver{
+    public static void main(String[] args) throws InterruptedException, IOException{
+        FileSearchWindow window = new FileSearchWindow();
+        window.getLatch().await(); // Used to make the program wait until latch has been released 
+        
+        FileReader fileReader = new FileReader(window.getFilePath());
+        
+        ParsingAlgorithm parseTranscript = new ParsingAlgorithm();
+        parseTranscript.parseTranscript(fileReader.getReadInPDF());
+        parseTranscript.handleCourseRepeats();
+        parseTranscript.handleTransferType();
+        parseTranscript.printCourses();
+    }
+}
